f3f7668500fbe0dfb6fa11b4cf0121e7d5cceb87
benp3837 <petruzziellbj@alumni.vcu.edu>
2026-02-23 13:21:51 -0500

    state setup

--- DinoAPI/app/services/langgraph_service.py
+++ DinoAPI/app/services/langgraph_service.py
@@ -1,4 +1,4 @@
-from typing import TypedDict
+from typing import TypedDict, Any
 
 from langchain_ollama import ChatOllama
 
@@ -14,4 +14,9 @@ llm = ChatOllama(
 # Like in React, State holds data that we want to keep track of
 # Each Node in the Graph can read from and write to the State
 class GraphState(TypedDict, total=False): #total=False makes all fields optional
+    query:str # The user's input to the graph
+    route:str # The "routing decision" we make. This tells the app what to invoke next
+    docs:list[dict[str, Any]] # Results returned from VectorDB searches
+    answer:str # The LLM's answer to the user's query
+    #TODO: memory manager field
 
